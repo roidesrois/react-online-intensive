16ba8c07b951e4863e969c983587d4387d76d6db
prochor <prochorz@gmail.com>
2019-01-23 22:14:41 +0200

    first commit

---
+++ source/components/Feed/index.js
@@ -0,0 +1,23 @@
+// Core
+import React, { Component } from 'react';
+
+//Components
+import Composer from 'components/Composer';
+import Post from 'components/Post';
+import StatusBar from 'components/StatusBar';
+
+// Instruments
+import Styles from './styles.m.css';
+
+export default class Feed extends Component {
+    render () {
+        return (
+            <section className = { Styles.feed }>
+                <StatusBar/>
+                <Composer/>
+                <Post/>
+            </section>
+        );
+    }
+}
+
