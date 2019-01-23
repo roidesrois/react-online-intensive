1d32d81ce975337e1fe8da5c2bda77a65728531f
prochor <prochorz@gmail.com>
2019-01-23 23:12:26 +0200

    HW 3 - done

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,11 +1,29 @@
 // Core
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class StatusBar extends Component {
+    static propTypes ={
+        avatar: PropTypes.string,
+        currenUserFirstName: PropTypes.string.isRequired,
+        currenUserLastName: PropTypes.string.isRequired,
+    };
+
     render () {
-        return <section className={ Styles.statusBar}/>
+        const {
+            avatar,
+            currenUserFirstName,
+            currenUserLastName,
+        } = this.props;
+
+        return <section className={ Styles.statusBar}>
+            <button>
+                <img src ={avatar}/>
+                <span>{`${currenUserFirstName}`}&nbsp;{`${currenUserLastName}`}</span>
+            </button>
+        </section>
     }
 }
