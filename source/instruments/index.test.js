305312f7eef840a0628dd0a2e87ff1b196ca3785
prochor <prochorz@gmail.com>
2019-02-21 08:24:06 +0200

    HW 11 - Done

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,5 +1,5 @@
 //Core
-import { sum, delay, getUniqueID } from './';
+import { sum, delay, getUniqueID, getFullApiUrl } from './';
 
 jest.setTimeout(10000);
 
@@ -21,10 +21,13 @@ describe('instruments', ()=>{
         expect(sum(1, 2 )).toMatchSnapshot();
     });
 
+
+    //--delay
     test('delay function return a resolved promise', async ()=>{
         await expect( delay() ).resolves.toBeUndefined();
     });
 
+    //--getUniqueID
     test('getUniqueID function should be a function',  ()=>{
         expect(getUniqueID).toBeInstanceOf(Function);
     });
@@ -35,5 +38,20 @@ describe('instruments', ()=>{
         expect( typeof getUniqueID() ).toBe('string');
         expect( getUniqueID(5) ).toHaveLength(5);
         expect( getUniqueID(13) ).toHaveLength(13);
+    });
+
+    //--getFullApiUrl
+    test('getFullApiUrl function should be a function',  ()=>{
+        expect(getFullApiUrl).toBeInstanceOf(Function);
+    });
+    test('getFullApiUrl function should throw, when called with non-number type as first argument', ()=>{
+        expect(()=>getFullApiUrl(3, 'hi' )).toThrow();
+    });
+    test('getFullApiUrl function should throw, when called with non-number type as second argument', ()=>{
+        expect(()=>getFullApiUrl('hi', 3 )).toThrow();
+    });
+    test('getFullApiUrl function should produce a string of a desired give length',  ()=>{
+        expect( typeof getFullApiUrl('api','id') ).toBe('string');
+        expect( getFullApiUrl('api','id') ).toHaveLength(6);
     });
 });
